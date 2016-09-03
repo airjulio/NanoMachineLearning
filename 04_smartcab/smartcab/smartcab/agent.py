e4697c41125ed81a998d342c4842d71fc06a57ba
Julio Rodrigues <juliocezar.rodrigues@gmail.com>
2016-09-03 10:03:27 -0400

    Init agent

--- 04_smartcab/smartcab/smartcab/agent.py
+++ 04_smartcab/smartcab/smartcab/agent.py
@@ -6,6 +6,8 @@ from simulator import Simulator
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
+    actions_values = {'forward': 10, 'left': 100, 'right': 1000, 'red': -1, 'green': 1, None: 1}
+
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
@@ -23,9 +25,10 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        self.state = self.calculate_state(inputs, self.next_waypoint, deadline)
+        print 'STATE:', self.state
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice(self.env.valid_actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -34,6 +37,15 @@ class LearningAgent(Agent):
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
+    def calculate_state(self, raw_state_dict, next_waypoint, deadline):
+        """
+        Generate unique values for each combination of the input.
+        """
+        light = self.actions_values[raw_state_dict['light']]
+        oncoming = 1 if raw_state_dict['oncoming'] is None else self.actions_values[raw_state_dict['oncoming']]
+        right = 5 if raw_state_dict['right'] is None else self.actions_values[raw_state_dict['right']] * 2
+        left = 7 if raw_state_dict['left'] is None else self.actions_values[raw_state_dict['left']] * 3
+        return light * (oncoming + right + left), self.actions_values[next_waypoint], deadline
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -41,7 +53,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
